71cd9f583631fdb525279a881d6a84e50da4f612
hector <hector.guedesm2006@gmail.com>
2024-03-20 16:13:25 -0300

    exercicios 3 e 4 resolvidos

---
+++ exercicio3.js
@@ -0,0 +1,7 @@
+let prompt = require('prompt-sync')();
+n1 = parseFloat(prompt('nota 1: '));
+n2 = parseFloat(prompt('nota 2: '));
+media = (n1 + n2)/2
+if (media >=6.0){
+    console.log ('parabéns foi aprovado ')
+}
