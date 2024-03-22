17d2693e96e8b1f0d84efed4e2391e7108749881
hector <hector.guedesm2006@gmail.com>
2024-03-22 16:41:33 -0300

    exercicio 21 e 22 resolvidos

--- exercicio22.js
+++ exercicio22.js
@@ -1,9 +1,9 @@
 let prompt = require('prompt-sync')();
 n1 = parseInt(prompt('Digite um número: '));
 n2 = parseInt(prompt('Digite outro número: '));
-while (n2 === 0){
-    i = parseInt(prompt('Digite um valor diferente de zero: '));
-    n2 = i
+if (n2 === 0){
+    console.log ('digite um valor diferente de zero')
 }
-
-console.log(n1 / n2);
+else{
+    console.log(n1 / n2);
+}
